f67e007362a47faa39373e8ece03b85b24c3cac9
Sephorah ANIAMBOSSOU <sephorah.aniambossou@epitech.eu>
2026-06-21 14:33:08 -0400

    fix(malloc): free list walk

--- src/free.c
+++ src/free.c
@@ -10,20 +10,19 @@ extern size_t *heap_start;
 void check_valid_list()
 {
     size_t *block_tmp = NULL;
-    size_t *block_tmp_prev = NULL;
+    // size_t *block_tmp_prev = NULL;
     size_t *block_tmp_next = NULL;
-
     if (*heap_start == 0)
     {
         return;
     }
-    fprintf(stderr, "CHECK VALID LIST\nFirst element %ld\n", *heap_start);
+    // fprintf((stderr, "CHECK VALID LIST\nHeap start %ld First element %ld\n", (size_t)heap_start, *heap_start);
     block_tmp = (size_t *)(*heap_start);
     while (block_tmp != NULL)
     {
-        block_tmp_prev = (size_t *)(block_tmp + 1);
+        // block_tmp_prev = (size_t *)(block_tmp + 1);
         block_tmp_next = (size_t *)((size_t)block_tmp);
-        fprintf(stderr, "Current block %ld is allocated %d size %ld next %ld prev %ld\n", (size_t)block_tmp, is_allocated((header_t *)((size_t)block_tmp - HEADER_SIZE)), get_size((header_t *)((size_t)block_tmp - HEADER_SIZE)), (*block_tmp_next), (*block_tmp_prev));
+        // fprintf((stderr, "Current block %ld is allocated %d size %ld next %ld prev %ld\n", (size_t)block_tmp, is_allocated((header_t *)((size_t)block_tmp - HEADER_SIZE)), get_size((header_t *)((size_t)block_tmp - HEADER_SIZE)), (*block_tmp_next), (*block_tmp_prev));
 
         // CHECK IF HEADER == FOOTER;
 
@@ -32,15 +31,15 @@ void check_valid_list()
         //     block_tmp_header = (header_t *)((size_t)block_tmp - HEADER_SIZE);
         //     *(header_t *)block_tmp_header = *(header_t *)block_tmp_header | (1 << 0); // MAKE FUNCTION FOR SETTING AND CLEAR BIT
         //     size_tmp = get_size(block_tmp_header);
-        //     // fprintf(stderr, "[reused block] %p (%ld) - %p (%ld) : %ld bytes\n", block_tmp_header, (size_t)block_tmp_header, (size_t *)((size_t)block_tmp_header + size_tmp), (size_t)((size_t)block_tmp_header + size_tmp), size_tmp);
+        //     // ////fprintf((stderr, "[reused block] %p (%ld) - %p (%ld) : %ld bytes\n", block_tmp_header, (size_t)block_tmp_header, (size_t *)((size_t)block_tmp_header + size_tmp), (size_t)((size_t)block_tmp_header + size_tmp), size_tmp);
 
         //     *heap_start = *block_tmp_next;
         //     return block_tmp;
         // }
         block_tmp = (size_t *)(*block_tmp_next);
     }
-    fprintf(stderr, "---------- NO PROBLEM\n");
-    // return NULL;
+    // fprintf((stderr, "---------- NO PROBLEM\n");
+    //  return NULL;
 }
 
 void update_next_neighbor(size_t *block_header)
@@ -49,7 +48,7 @@ void update_next_neighbor(size_t *block_header)
     size_t *current_next_block = (size_t *)*((size_t *)((size_t)block_header + HEADER_SIZE));
     size_t *new_next_block = NULL;
 
-    fprintf(stderr, "In update next neighbor %ld %ld\n", (size_t)current_next_block, (size_t)current_next_block);
+    ////fprintf((stderr, "In update next neighbor %ld %ld\n", (size_t)current_next_block, (size_t)current_next_block);
 
     if ((size_t)current_next_block == 0)
     {
@@ -57,15 +56,15 @@ void update_next_neighbor(size_t *block_header)
     }
     new_next_block = (size_t *)*current_next_block;
 
-    fprintf(stderr, "Update next neighbor block header %ld current next block %ld size %ld new next block %ld\n", (size_t)block_header, (size_t)current_next_block, get_size(current_next_block), (size_t)new_next_block);
+    ////fprintf((stderr, "Update next neighbor block header %ld current next block %ld size %ld new next block %ld\n", (size_t)block_header, (size_t)current_next_block, get_size(current_next_block), (size_t)new_next_block);
     // exit(0);
     *current_next_block = *new_next_block;
-    fprintf(stderr, "After update current next block value %ld\n", *current_next_block);
+    ////fprintf((stderr, "After update current next block value %ld\n", *current_next_block);
 }
 
 void check_next_block(size_t *current_block_header, size_t *current_block_footer)
 {
-    fprintf(stderr, "\ncurrent block header %ld footer %ld\n", (size_t)current_block_header, (size_t)current_block_footer);
+    ////fprintf((stderr, "\ncurrent block header %ld footer %ld\n", (size_t)current_block_header, (size_t)current_block_footer);
     header_t *next_block_header = (size_t *)*(size_t *)((size_t)current_block_header + HEADER_SIZE);
     header_t *next_block_footer = NULL;
 
@@ -79,20 +78,20 @@ void check_next_block(size_t *current_block_header, size_t *current_block_footer
 
     next_block_footer = (header_t *)((size_t)next_block_header + get_size(next_block_header) - HEADER_SIZE);
 
-    fprintf(stderr, "Next header %ld footer %ld size %ld %ld\n", (size_t)next_block_header, (size_t)next_block_footer, get_size(next_block_header), get_size(next_block_footer));
+    ////fprintf((stderr, "Next header %ld footer %ld size %ld %ld\n", (size_t)next_block_header, (size_t)next_block_footer, get_size(next_block_header), get_size(next_block_footer));
     *(size_t *)(next_block_header + HEADER_SIZE) = 0;
     *(size_t *)(next_block_header + HEADER_SIZE + 8) = 0;
-    fprintf(stderr, "HEYYYYYYYYY next %ld prev %ld\n", *(size_t *)(next_block_header + HEADER_SIZE), *(size_t *)(next_block_header + HEADER_SIZE + 8));
-    // fprintf(stderr, "HEEEEEEEEEEEEEEEEEEY %ld %ld\n", get_size(next_block_header), get_size(current_block_header));
+    ////fprintf((stderr, "HEYYYYYYYYY next %ld prev %ld\n", *(size_t *)(next_block_header + HEADER_SIZE), *(size_t *)(next_block_header + HEADER_SIZE + 8));
+    // ////fprintf((stderr, "HEEEEEEEEEEEEEEEEEEY %ld %ld\n", get_size(next_block_header), get_size(current_block_header));
 
-    fprintf(stderr, "Next block header %ld size next block %ld footer %ld size footer %ld\n", (size_t)next_block_header, get_size(next_block_header), (size_t)next_block_footer, get_size(next_block_footer));
+    ////fprintf((stderr, "Next block header %ld size next block %ld footer %ld size footer %ld\n", (size_t)next_block_header, get_size(next_block_header), (size_t)next_block_footer, get_size(next_block_footer));
     *next_block_footer = get_size(next_block_header) + get_size(current_block_header);
     *current_block_header = *next_block_footer;
     update_next_neighbor(current_block_header);
-    check_valid_list();
-    fprintf(stderr, "New size next block footer %ld %ld\n", *next_block_footer, *current_block_header);
+    // check_valid_list();
+    ////fprintf((stderr, "New size next block footer %ld %ld\n", *next_block_footer, *current_block_header);
 
-    // fprintf(stderr, "New sizes %ld %ld\n", *next_block_footer, *current_block_header);
+    // ////fprintf((stderr, "New sizes %ld %ld\n", *next_block_footer, *current_block_header);
 
     *next_block_header = 0;
     *current_block_footer = 0;
@@ -117,7 +116,8 @@ size_t *merge_free_blocks(size_t *current_block)
     // header_t *current_block_footer = (header_t *)((size_t)current_block + get_size(current_block_header) - HEADER_SIZE * 2);
 
     // Voir si les addresses sont correctes + si le footer est bien pris en compte dans malloc
-    if (prev_block != 0) {
+    if (prev_block != 0)
+    {
         if ((size_t)prev_block + get_size((size_t *)(*prev_block - HEADER_SIZE)) != (size_t)current_block - HEADER_SIZE)
         {
             return current_block;
@@ -126,44 +126,45 @@ size_t *merge_free_blocks(size_t *current_block)
         prev_block_header = (header_t *)((size_t)prev_block_footer - get_size(prev_block_footer) + HEADER_SIZE);
         current_block_header = (header_t *)((size_t)current_block - HEADER_SIZE);
         current_block_footer = (header_t *)((size_t)current_block + get_size(current_block_header) - HEADER_SIZE * 2);
-        fprintf(stderr, "\n\nPREV FREE | current block %ld\n", (size_t)current_block);
-    
-        fprintf(stderr, "Prev block header %ld size prev block %ld footer %ld size footer %ld\n", (size_t)prev_block_header, get_size(prev_block_header), (size_t)prev_block_footer, get_size(prev_block_footer));
-        fprintf(stderr, "Current block header %ld size prev block %ld footer %ld size footer %ld\n", (size_t)current_block_header, get_size(current_block_header), (size_t)current_block_footer, get_size(current_block_footer));
-    
+        ////fprintf((stderr, "\n\nPREV FREE | current block %ld\n", (size_t)current_block);
+
+        ////fprintf((stderr, "Prev block header %ld size prev block %ld footer %ld size footer %ld\n", (size_t)prev_block_header, get_size(prev_block_header), (size_t)prev_block_footer, get_size(prev_block_footer));
+        ////fprintf((stderr, "Current block header %ld size prev block %ld footer %ld size footer %ld\n", (size_t)current_block_header, get_size(current_block_header), (size_t)current_block_footer, get_size(current_block_footer));
+
         *current_block_footer = get_size(prev_block_footer) + get_size(current_block_header);
         *prev_block_header = *current_block_footer;
-    
-        fprintf(stderr, "Old current size %ld\n", get_size(current_block_header));
+
+        ////fprintf((stderr, "Old current size %ld\n", get_size(current_block_header));
         // *prev_block_footer = 0;
         // *current_block_header = 0;
-    
+
         *current_block = 0;
         *(current_block + 1) = 0;
-        fprintf(stderr, "BEFORE CURRENT BLOCK header %ld payload %ld end %ld\n", (size_t)current_block_header, (size_t)current_block, (size_t)current_block_footer);
+        ////fprintf((stderr, "BEFORE CURRENT BLOCK header %ld payload %ld end %ld\n", (size_t)current_block_header, (size_t)current_block, (size_t)current_block_footer);
         current_block = (size_t *)((size_t)prev_block_header + HEADER_SIZE);
-    
+
         current_block_header = prev_block_header;
         update_next_neighbor(current_block_header);
         // *current_block = 0;
         // *(current_block + 1) = 0;
-        check_valid_list();
-        fprintf(stderr, "Prev block header %ld size prev block %ld footer %ld size footer %ld\n", (size_t)prev_block_header, get_size(prev_block_header), (size_t)prev_block_footer, get_size(prev_block_footer));
-    
-        fprintf(stderr, "AFTER CURRENT BLOCK header %ld payload %ld end %ld\n", (size_t)current_block_header, (size_t)current_block, (size_t)current_block_footer);
-        fprintf(stderr, "AGAIN next %ld prev %ld", (*current_block), *(current_block + 1));
-    
-        fprintf(stderr, "Values Current block header %ld size prev block %ld footer %ld size footer %ld\n", *current_block_header, get_size(current_block_header), *prev_block_footer, get_size(prev_block_footer));
-        fprintf(stderr, "New block size %ld %ld\n", *current_block_footer, *prev_block_header);
+        // check_valid_list();
+        ////fprintf((stderr, "Prev block header %ld size prev block %ld footer %ld size footer %ld\n", (size_t)prev_block_header, get_size(prev_block_header), (size_t)prev_block_footer, get_size(prev_block_footer));
+
+        ////fprintf((stderr, "AFTER CURRENT BLOCK header %ld payload %ld end %ld\n", (size_t)current_block_header, (size_t)current_block, (size_t)current_block_footer);
+        ////fprintf((stderr, "AGAIN next %ld prev %ld", (*current_block), *(current_block + 1));
+
+        ////fprintf((stderr, "Values Current block header %ld size prev block %ld footer %ld size footer %ld\n", *current_block_header, get_size(current_block_header), *prev_block_footer, get_size(prev_block_footer));
+        ////fprintf((stderr, "New block size %ld %ld\n", *current_block_footer, *prev_block_header);
         // exit(0);
     }
 
     // current_block_footer =
     // if (current_block)
-    if (next_block != 0) {
+    if (next_block != 0)
+    {
         check_next_block(current_block_header, current_block_footer);
     }
-    // fprintf(stderr, "AFTER CURRENT BLOCK header %p payload %p end %p\n\n", current_block_header, updated_current_block, current_block_footer);
+    // ////fprintf((stderr, "AFTER CURRENT BLOCK header %p payload %p end %p\n\n", current_block_header, updated_current_block, current_block_footer);
     return current_block;
 }
 
@@ -172,16 +173,16 @@ void add_block_free_list(size_t *ptr)
     size_t *ptr_prev = NULL;
     size_t *old_block_prev = NULL;
     // size_t *old_block_next = NULL;
-    fprintf(stderr, "Adding %ld block\n", (size_t)ptr);
+    ////fprintf((stderr, "Adding %ld block\n", (size_t)ptr);
     if (heap_start == NULL)
     {
         return;
     }
     ptr_prev = (size_t *)(ptr + 1);
-    fprintf(stderr, "before %ld prev %ld\n", *ptr, *ptr_prev);
+    ////fprintf((stderr, "before %ld prev %ld\n", *ptr, *ptr_prev);
     if (*heap_start != 0)
     {
-        fprintf(stderr, "HEAP START %ld VALUE %ld + 8 %ld\n", (size_t)heap_start, *heap_start, (*heap_start + 8));
+        ////fprintf((stderr, "HEAP START %ld VALUE %ld + 8 %ld\n", (size_t)heap_start, *heap_start, (*heap_start + 8));
         *ptr = *heap_start;
         *ptr_prev = (size_t)heap_start;
         old_block_prev = (size_t *)(*heap_start + 8);
@@ -191,20 +192,20 @@ void add_block_free_list(size_t *ptr)
     else
     {
         *ptr = 0;
-        *ptr_prev = 0;
+        *ptr_prev = (size_t)heap_start;
     }
-    // fprintf(stderr, "After Next %ld prev %ld\n", *ptr, *ptr_prev);
+    // ////fprintf((stderr, "After Next %ld prev %ld\n", *ptr, *ptr_prev);
     *heap_start = (size_t)ptr;
-    // check_valid_list();
-    // fprintf(stderr, "UPDATED HEAP START VALUE %ld\n\n", *heap_start);
+    // //check_valid_list();
+    // ////fprintf((stderr, "UPDATED HEAP START VALUE %ld\n\n", *heap_start);
 }
 
 void free(void *ptr)
 {
-    // fprintf(stderr, "------------FREE WITH %ld\n", (size_t)ptr);
+    // ////fprintf((stderr, "------------FREE WITH %ld\n", (size_t)ptr);
     header_t *header_address = NULL;
     // size_t *updated_ptr = NULL;
-    // check_valid_list();
+    // //check_valid_list();
     if (ptr == NULL)
     {
         return;
@@ -214,7 +215,7 @@ void free(void *ptr)
     {
         return;
     }
-    // fprintf(stderr, "[freed block] %p (%ld) : %ld bytes\n", header_address, (size_t)header_address, get_size(header_address));
+    // ////fprintf((stderr, "[freed block] %p (%ld) : %ld bytes\n", header_address, (size_t)header_address, get_size(header_address));
     *header_address = CLEAR_BIT(*header_address);
     add_block_free_list(ptr);
     merge_free_blocks(ptr);
--- src/malloc.c
+++ src/malloc.c
@@ -15,24 +15,25 @@ size_t get_aligned_block_size(size_t payload_size)
     return res;
 }
 
-void *add_optional_padding(size_t size)
+size_t *add_optional_padding(size_t payload_size, void *start)
 {
-    void *optional_padding = sbrk(size);
+    // void *optional_padding = sbrk(size);
 
-    if (optional_padding == (void *)-1)
-    {
-        perror("Error when allocating optional padding");
-        return NULL;
-    }
-    // // fprintf(stderr, "\t[optional padding] %p (%ld) - %p (%ld)\n", optional_padding, (size_t)optional_padding, (size_t *)((size_t)optional_padding + size), ((size_t)optional_padding + size));
-    return optional_padding;
+    // if (optional_padding == (void *)-1)
+    // {
+    //     perror("Error when allocating optional padding");
+    //     return NULL;
+    // }
+    // // // ////fprintf((stderr, "\t[optional padding] %p (%ld) - %p (%ld)\n", optional_padding, (size_t)optional_padding, (size_t *)((size_t)optional_padding + size), ((size_t)optional_padding + size));
+    // return optional_padding;
+    return (size_t *)((size_t)start + payload_size);
 }
 
 void *find_free_block(size_t size)
 {
     size_t *block_tmp = NULL;
     header_t *block_tmp_header = NULL;
-    // size_t *block_tmp_prev = NULL;
+    size_t *block_tmp_prev = NULL;
     size_t *block_tmp_next = NULL;
     size_t size_tmp = 0;
 
@@ -46,130 +47,146 @@ void *find_free_block(size_t size)
 
     while (block_tmp != NULL)
     {
-        // block_tmp_prev = (size_t *)(block_tmp + 1);
+        block_tmp_header = (header_t *)((size_t)block_tmp - HEADER_SIZE);
+        size_tmp = get_size(block_tmp_header);
+        block_tmp_prev = (size_t *)(block_tmp + 1);
         block_tmp_next = (size_t *)((size_t)block_tmp);
 
         if (size_tmp >= size)
         {
             block_tmp_header = (header_t *)((size_t)block_tmp - HEADER_SIZE);
             *(header_t *)block_tmp_header = *(header_t *)block_tmp_header | (1 << 0); // MAKE FUNCTION FOR SETTING AND CLEAR BIT
-            size_tmp = get_size(block_tmp_header);
-            // fprintf(stderr, "[reused block] %p (%ld) - %p (%ld) : %ld bytes\n", block_tmp_header, (size_t)block_tmp_header, (size_t *)((size_t)block_tmp_header + size_tmp), (size_t)((size_t)block_tmp_header + size_tmp), size_tmp);
-
-            *heap_start = *block_tmp_next;
+            // ////fprintf((stderr, "[reused block] %p (%ld) - %p (%ld) : %ld bytes\n", block_tmp_header, (size_t)block_tmp_header, (size_t *)((size_t)block_tmp_header + size_tmp), (size_t)((size_t)block_tmp_header + size_tmp), size_tmp);
+            
+            *(size_t *)(*block_tmp_prev) = *block_tmp_next;
+            if (*block_tmp_next != 0) {
+                *(size_t *)(*block_tmp_next + 8) = *block_tmp_prev;
+            }
             return block_tmp;
         }
         block_tmp = (size_t *)(*block_tmp_next);
-        block_tmp_header = (header_t *)((size_t)block_tmp - HEADER_SIZE);
     }
     return NULL;
 }
 
-header_t *init_boundary_tag(size_t size)
+header_t *init_boundary_tag(header_t *tag_start, size_t size)
 {
-    header_t *tag_start = sbrk(HEADER_SIZE);
+    //fprintf((stderr, "TAG START %ld START %ld\n", (size_t)tag_start, (size_t)start);
+    *tag_start = size;
+    *tag_start = *(header_t *)tag_start | (1 << 0); // set bit position 0
+    return tag_start;
+    // // ////fprintf((stderr, "\t[header] %p (%ld) - %p (%ld): %ld %lb\n", tag_start, (size_t)tag_start, (size_t *)((size_t)tag_start + HEADER_SIZE), ((size_t)tag_start + HEADER_SIZE), *tag_start, *tag_start);
+}
 
-    if (tag_start == (void *)-1)
+void *allocate_block(size_t size)
+{
+    void *block_start = sbrk(size);
+
+    if (block_start == (void *)-1)
     {
         perror("Error when allocating header");
         return NULL;
     }
-    *tag_start = size;
-    *(header_t *)tag_start = *(header_t *)tag_start | (1 << 0); // set bit position 0
-    // // fprintf(stderr, "\t[header] %p (%ld) - %p (%ld): %ld %lb\n", tag_start, (size_t)tag_start, (size_t *)((size_t)tag_start + HEADER_SIZE), ((size_t)tag_start + HEADER_SIZE), *tag_start, *tag_start);
-    return tag_start;
+    return block_start;
 }
 
-void *init_payload(size_t size)
+void *get_payload_start(void *start)
 {
-    void *payload_start = sbrk(size);
-
-    if (payload_start == (void *)-1)
-    {
-        perror("Error when allocating payload");
-        return NULL;
-    }
-    // // fprintf(stderr, "\t[payload] %p (%ld) - %p (%ld)\n", payload_start, (size_t)payload_start, (size_t *)((size_t)payload_start + size), ((size_t)payload_start + size));
-    return payload_start;
+    // // ////fprintf((stderr, "\t[payload] %p (%ld) - %p (%ld)\n", payload_start, (size_t)payload_start, (size_t *)((size_t)payload_start + size), ((size_t)payload_start + size));
+    return start + HEADER_SIZE;
 }
 
 void *init_epilogue()
 {
-    header_t *header_start = init_boundary_tag(HEADER_SIZE);
+    void *block_start = allocate_block(HEADER_SIZE);
 
-    if (header_start == NULL)
+    if (block_start == NULL)
     {
         return NULL;
     }
-    // fprintf(stderr, "[new block] %p (%ld) - %p (%ld): %d bytes\n", header_start, (size_t)header_start, (size_t *)((size_t)header_start + HEADER_SIZE), (size_t)header_start + HEADER_SIZE, HEADER_SIZE);
-    return header_start + HEADER_SIZE;
+    //fprintf((stderr, "    2Footer start %ld current break %ld\n", (size_t)block_start, (size_t)get_current_break());
+    return init_boundary_tag(block_start, HEADER_SIZE);
+    // ////fprintf((stderr, "[new block] %p (%ld) - %p (%ld): %d bytes\n", header_start, (size_t)header_start, (size_t *)((size_t)header_start + HEADER_SIZE), (size_t)header_start + HEADER_SIZE, HEADER_SIZE);
+    // return header_start + HEADER_SIZE;
 }
 
 void *init_block(size_t payload_size)
 {
     size_t block_size = get_aligned_block_size(payload_size);
     size_t optional_padding_size = block_size - (HEADER_SIZE * 2 + payload_size);
-    header_t *header_start = init_boundary_tag(block_size);
+    header_t *block_start = allocate_block(block_size);
+    header_t *header_start = NULL;
     header_t *footer_start = NULL;
-    size_t *block_end = (size_t *)((size_t)header_start + block_size);
+    // size_t *block_end = NULL;
     void *payload_start = NULL;
     void *optional_padding = NULL;
 
-    if (header_start == NULL)
+    if (block_start == NULL)
     {
         return NULL;
     }
-    payload_start = init_payload(payload_size);
-    fprintf(stderr, "    Payload start %p\n", payload_start);
+    //fprintf((stderr, "    Block start %ld\n", (size_t)block_start);
+    header_start = init_boundary_tag(block_start, block_size);
+    //fprintf((stderr, "    Header start %ld\n", (size_t)header_start);
+    // block_end = (size_t *)((size_t)header_start + block_size);
+    payload_start = get_payload_start(header_start);
+    //fprintf((stderr, "    Payload start %ld\n", (size_t)payload_start);
     if (payload_start == NULL)
     {
         return NULL;
     }
-    optional_padding = add_optional_padding(optional_padding_size);
-    fprintf(stderr, "    Optional padding %p\n", optional_padding);
+    optional_padding = add_optional_padding(payload_size, payload_start);
+    //fprintf((stderr, "    Optional padding %ld\n", (size_t)optional_padding);
     if (optional_padding == NULL)
     {
         return NULL;
     }
-    footer_start = init_boundary_tag(block_size);
-    fprintf(stderr, "    Footer start %p\n", footer_start);
+    footer_start = init_boundary_tag(optional_padding + optional_padding_size, block_size);
+    //fprintf((stderr, "    Footer start %ld\n", (size_t)footer_start);
     if (footer_start == NULL)
     {
         return NULL;
     }
-    fprintf(stderr, "[new block] %p (%ld) - %p (%ld): %ld bytes\n", header_start, (size_t)header_start, block_end, (size_t)block_end, block_size);
+    //fprintf((stderr, "[new block] %p (%ld) - %p (%ld): %ld bytes\n", header_start, (size_t)header_start, block_end, (size_t)block_end, block_size);
     return payload_start;
 }
 
 void *add_free_block(size_t payload_size)
 {
     header_t *new_block_header_address = (header_t *)((size_t)get_current_break() - HEADER_SIZE);
-    void *new_heap_end = NULL;
+    // void *new_heap_end = NULL;
     void *new_block_payload = NULL;
-    size_t *block_end = NULL;
+    // size_t *block_end = NULL;
     size_t block_size = 0;
     size_t optional_padding_size = 0;
-
-    size_t *footer_start = 0;
-    if (footer_start)
-    {
-    }
+    void *optional_padding = NULL;
+    // size_t *footer_start = NULL;
+    // if (footer_start)
+    // {
+    // }
 
     if (new_block_header_address != NULL)
     {
         block_size = get_aligned_block_size(payload_size);
-        block_end = (size_t *)((size_t)new_block_header_address + block_size);
-        if (block_end && new_heap_end)
-        {
-        };
+        // block_end = (size_t *)((size_t)new_block_header_address + block_size);
+        //fprintf((stderr, "[heap end into new block] %p (%ld) - %p (%ld): %ld bytes\n", new_block_header_address, (size_t)new_block_header_address, block_end, (size_t)block_end, block_size);
+        // if (block_end && new_heap_end)
+        // {
+        // };
         *new_block_header_address = block_size;
-        new_block_payload = init_payload(payload_size);
+        *new_block_header_address = *(header_t *)new_block_header_address | (1 << 0); // set bit position 0
+        new_block_payload = allocate_block(block_size - HEADER_SIZE);
+        //fprintf((stderr, "-------NEW SIZE %ld NEW BLOCK HEADER ADDRESS %ld NEW BLOCK PAYLOAD %ld size %ld\n", block_size, (size_t)new_block_header_address, (size_t)new_block_payload, payload_size);
+        // //fprintf((stderr, " Payload start %ld\n", (size_t)new_block_payload);
+
         optional_padding_size = block_size - (HEADER_SIZE * 2 + payload_size);
-        add_optional_padding(optional_padding_size);
-        footer_start = init_boundary_tag(block_size);
-        // fprintf(stderr, "[heap end into new block] %p (%ld) - %p (%ld): %ld bytes\n", new_block_header_address, (size_t)new_block_header_address, block_end, (size_t)block_end, block_size);
-        new_heap_end = init_epilogue();
-        // fprintf(stderr, "heap end : %p\n", (size_t *)((size_t)new_heap_end - HEADER_SIZE));
+        optional_padding = add_optional_padding(payload_size, new_block_payload);
+        //fprintf((stderr, "    Optional padding %ld size %ld\n", (size_t)optional_padding, optional_padding_size);
+        // footer_start = 
+        init_boundary_tag(optional_padding + optional_padding_size, block_size);
+        //fprintf((stderr, "    Footer start %ld current break %ld\n", (size_t)footer_start, (size_t)get_current_break());
+        init_epilogue();
+        // ////fprintf((stderr, "heap end : %p\n", (size_t *)((size_t)new_heap_end - HEADER_SIZE));
         return new_block_payload;
     }
     return NULL;
@@ -181,31 +198,31 @@ void init_heap(void)
     {
         return;
     }
-    // fprintf(stderr, "----- Initiliazing the heap -----\n");
+    //fprintf((stderr, "----- Initiliazing the heap -----\n");
     heap_start = init_block(8);
-    // fprintf(stderr, "break : %p\n", heap_start);
+    //fprintf((stderr, "break : %p\n", heap_start);
     size_t *heap_end = init_epilogue();
     if (heap_end)
     {
     };
-    // fprintf(stderr, "heap end : %p\n", heap_end);
-    // fprintf(stderr, "----- Heap initialized -----\n");
+    // ////fprintf((stderr, "heap end : %p\n", heap_end);
+    // ////fprintf((stderr, "----- Heap initialized -----\n");
 }
 
 void *malloc(size_t size)
 {
     void *free_block = NULL;
     size_t block_size = 0;
-    // fprintf(stderr, "--------------------NEW MALLOC\n");
+    // ////fprintf((stderr, "--------------------NEW MALLOC\n");
     init_heap();
     if (heap_start == NULL)
     {
         perror("Error when initializing heap");
         return NULL;
     }
-    // fprintf(stderr, "--------------------AFTER INITIATING HEAP\n");
+    // //fprintf((stderr, "--------------------AFTER INITIATING HEAP\n");
 
-    // check_valid_list();
+    //check_valid_list();
     block_size = get_aligned_block_size(size);
     free_block = find_free_block(block_size);
     if (free_block == NULL)
